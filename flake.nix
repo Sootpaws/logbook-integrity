d54e4ae5642767c53e01598d71d9eb6facd00517
Sootpaws <sootpaws@proton.me>
2024-06-05 10:57:22 -0700

    New Rust/Nix project

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "A command-line tool for validating journal entries";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system: let
+            pkgs = nixpkgs.legacyPackages.${system};
+        in {
+            defaultPackage = pkgs.rustPlatform.buildRustPackage {
+                pname = "logbook-integrity";
+                version = "0.1.0";
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
+            };
+        });
+}
