af749ba83e9caccf7aeb76cf27fb1f8e0af1cc1f
Sootpaws <sootpaws@proton.me>
2024-06-05 18:29:05 -0700

    Implement expect_value

--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,13 @@
 
+/// Return the next item of an iterator, or an error if there is none
+fn expect_value<'a>(
+    source: &mut impl Iterator<Item = &'a str>,
+    name: &str,
+    location: &str
+) -> Result<&'a str, String> {
+    source.next().ok_or(format!("no {name} in {location}"))
+}
+
 /// Expect a value as the next item of an iterator, returning an error if it
 /// doesn't match
 fn expect_literal<'a>(
