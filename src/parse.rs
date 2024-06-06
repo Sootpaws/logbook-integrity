6e80ede45aae757c1ca1114be9695f2bc74636e5
Sootpaws <sootpaws@proton.me>
2024-06-05 18:28:43 -0700

    Implement expect_literal

--- src/parse.rs
+++ src/parse.rs
@@ -1,1 +1,14 @@
 
+/// Expect a value as the next item of an iterator, returning an error if it
+/// doesn't match
+fn expect_literal<'a>(
+    source: &mut impl Iterator<Item = &'a str>,
+    value: &str,
+    name: &str,
+) -> Result<(), String> {
+    match source.next() {
+        Some(next) if next == value => Ok(()),
+        Some(other) => Err(format!("got {other} for {other}, expected {value}")),
+        None => Err(format!("no {name}, expected {value}")),
+    }
+}
