8b82e23d2cc499ad5f74e552978604a1b78b2498
Sootpaws <sootpaws@proton.me>
2024-06-05 18:29:58 -0700

    Implement parse_number

--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,10 @@
 
+/// Parse a string into a number
+fn parse_number(string: &str, name: &str) -> Result<u32, String> {
+    u32::from_str_radix(string, 10)
+        .map_err(|error| format!("bad {name} - {error}"))
+}
+
 /// Return the next item of an iterator, or an error if there is none
 fn expect_value<'a>(
     source: &mut impl Iterator<Item = &'a str>,
