2bfaa0d8a00bd825e280b15c1823b2a66663ff45
Sootpaws <sootpaws@proton.me>
2024-06-05 18:31:08 -0700

    Define text constants for parsing

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,24 @@
 use time::Date;
 use time::macros::format_description;
 
+/// Separator between components (page boundaries, entries, page headers)
+const COMPONENT_SEPARATOR: &str = "\n\n";
+
+/// Page boundary marker
+const PAGE_MARKER: &str = "-----";
+
+/// Start of the preamble entry rage
+const ENTRY_RANGE_START: &str = "Entries from ";
+
+/// Separator between the date and entry number of the preamble entry range
+const ENTRY_RANGE_MARK_SEPARATOR: &str = "-";
+
+/// Separator between the start and end marks of the preamble entry range
+const ENTRY_RANGE_SEPARATOR: &str = "to";
+
+/// Placeholder for the ending entry number of the preamble entry range
+const ENTRY_RANGE_PLACEHOLDER: &str = "_";
+
 /// Parse a string into a date
 fn parse_date(string: &str, name: &str) -> Result<Date, String> {
     Date::parse(
