b741c9cf5a76e7486eef56b57367babaf3583f2e
Sootpaws <sootpaws@proton.me>
2024-12-06 23:00:10 +0000

    Markov chain training

--- src/markov.rs
+++ src/markov.rs
@@ -1,5 +1,6 @@
 use crate::Logbook;
 use std::collections::HashMap;
+use std::iter;
 
 const PREFIX_TOKENS: usize = 2;
 
@@ -9,7 +10,7 @@ pub fn chain_from_logs(logs: &[Logbook]) -> Chain {
         .flat_map(|logbook| logbook.entries.iter())
         .flat_map(|entry| entry.contents.iter())
         .fold(Chain::new(), |mut chain, block| {
-            chain.feed(block, &["", ""]);
+            chain.feed(&block.text, ["", ""]);
             chain
         })
 }
@@ -25,5 +26,31 @@ impl Chain {
     /// Create a new, untrained chain
     pub fn new() -> Self {
         Self::default()
+    }
+
+    /// "Feed" a piece of text to the chain to train it
+    pub fn feed(&mut self, text: &str, preceding_tokens: [&str; PREFIX_TOKENS]) {
+        let mut preceding_tokens = preceding_tokens.map(str::to_owned);
+        for token in text.split(" ") {
+            self.add_link(token, &preceding_tokens);
+            preceding_tokens = preceding_tokens
+                .into_iter()
+                .skip(1)
+                .chain(iter::once(token.to_owned()))
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            dbg!(&preceding_tokens);
+        }
+    }
+
+    /// Add a single relation between a token and the preceding ones
+    fn add_link(&mut self, token: &str, preceding_tokens: &[String; PREFIX_TOKENS]) {
+        *self
+            .links
+            .entry(preceding_tokens.clone())
+            .or_default()
+            .entry(token.to_owned())
+            .or_insert(0) += 1;
     }
 }
